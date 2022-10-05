5b874e3cb60c10d4249a6ebaa65c7cf68406dda9
Martin Kucharek <kucharm2@gmail.com>
2022-10-05 19:35:11 -0400

    Assignment6 with minor changes to 3

--- Assignment3/train_model.py
+++ Assignment3/train_model.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error
 import joblib
+from sklearn.tree import DecisionTreeRegressor
 
 # Load our data set
 # df = pd.read_csv("../../Downloads/house_data.csv")
@@ -13,16 +14,29 @@ y = df["sale_price"]
 # Split the data set in a training set (75%) and a test set (25%)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 # Create the Linear Regression model
-model = LinearRegression()
+linear_model = LinearRegression()
+
+decision_model = DecisionTreeRegressor()
 # Train the model
-model.fit(X_train, y_train)
+linear_model.fit(X_train, y_train)
+decision_model.fit(X_train, y_train)
 # Save the trained model to a file so we can use it to make predictions later
-joblib.dump(model, 'house_value_model.pkl')
+joblib.dump(linear_model, 'linear_house_value_model.pkl')
+joblib.dump(decision_model, 'decision_house_value_model.pkl')
 # Report how well the model is performing
-print("Model training results:")
+print("Linear Regression Model training results:")
 # Report an error rate on the training set
-mse_train = mean_absolute_error(y_train, model.predict(X_train))
+mse_train = mean_absolute_error(y_train, linear_model.predict(X_train))
 print(f" - Training Set Error: {mse_train}")
 # Report an error rate on the test set
-mse_test = mean_absolute_error(y_test, model.predict(X_test))
+mse_test = mean_absolute_error(y_test, linear_model.predict(X_test))
+print(f" - Test Set Error: {mse_test}")
+
+# Report how well the model is performing
+print("Decision Tree Model training results:")
+# Report an error rate on the training set
+mse_train = mean_absolute_error(y_train, decision_model.predict(X_train))
+print(f" - Training Set Error: {mse_train}")
+# Report an error rate on the test set
+mse_test = mean_absolute_error(y_test, decision_model.predict(X_test))
 print(f" - Test Set Error: {mse_test}")
--- Assignment3/use_model.py
+++ Assignment3/use_model.py
@@ -2,7 +2,8 @@
 import joblib
 
 # Load our trained model
-model = joblib.load('house_value_model.pkl')
+linear_model = joblib.load('linear_house_value_model.pkl')
+decision_model = joblib.load('decision_house_value_model.pkl')
 # Define the house that we want to value (with the values in the same order as in the training data)
 house_1 = [
     2500,  # Size in Square Feet
@@ -15,12 +16,20 @@ homes = [
     house_1
 ]
 # Make a prediction for each house in the homes array (we only have one)
-home_values = model.predict(homes)
+linear_home_values = linear_model.predict(homes)
+decision_home_values = decision_model.predict(homes)
 # Since we are only predicting the price of one house, grab the first prediction returned
-predicted_value = home_values[0]
+linear_predicted_value = linear_home_values[0]
+decision_predicted_value = decision_home_values[0]
 # Print the results
-print("House details:")
+print("Linear Regression House details:")
 print(f"- {house_1[0]} sq feet")
 print(f"- {house_1[1]} bedrooms")
 print(f"- {house_1[2]} bathrooms")
-print(f"Estimated value: ${predicted_value:,.2f}")
+print(f"Estimated value: ${linear_predicted_value:,.2f}")
+
+print("Decision Tree House details:")
+print(f"- {house_1[0]} sq feet")
+print(f"- {house_1[1]} bedrooms")
+print(f"- {house_1[2]} bathrooms")
+print(f"Estimated value: ${decision_predicted_value:,.2f}")
